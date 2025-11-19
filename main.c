ab7b1d2cffd0cb17845b0e12e297c65461b0494c
ozllozlo <ozllozlo@sookmyung.ac.kr>
2025-11-20 00:53:08 +0900

    1118

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<time.h>
+#include "board.h"
+
+#define MAX_DIE 6
+
+int rolldie(void)
+{
+    return rand()%MAX_DIE+1;
+}
 
 int main(int argc, char *argv[])
 {
+    int cnt;
+    int pos;
+    int coinResult;
+    
   srand((unsigned)(time(NULL)));
   
   printf("===============================================\n");
@@ -12,7 +24,30 @@ int main(int argc, char *argv[])
   printf("====@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@====\n");
   printf("===============================================\n");
   
+  board_initBoard();
   
+  cnt=0;
+  pos=0;
+  coinResult=0;
+  do{
+        int die_result;
+        int coinResult;
+        
+        board_printBoardStatus();
+        
+        die_result = rolldie();
+        
+        pos+=die_result;
+        
+        printf("pos:%i(die:%i)\n", pos, die_result);
+        
+        coinResult+=board_getBoardCoin(pos);
+        
+        printf("coin:%i\n", coinResult);
+        
+        cnt++;
+        
+} while(cnt<5);
   
   
   printf("\n\n\n\n\n\n\n"); 
